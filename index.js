74262095ad35bee3426ac444e10de38d7cb677d6
Andrea Cardaci <cyrus.and@gmail.com>
2017-05-14 22:36:39 +0200

    WIP build HAR from event log

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const HAR = require('./lib/har');
 const Loader = require('./lib/loader');
+const Replay = require('./lib/replay');
 
 function run(urls, options = {}) {
     return new Loader(urls, options);
 }
 
-module.exports = {run};
+async function fromLog(url, log) {
+    const replay = new Replay(url, log);
+    const stats = await replay.load();
+    return HAR.create([stats]);
+}
+
+module.exports = {run, fromLog};
