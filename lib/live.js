cyrus-and/chrome-har-capturer

commit 15f0de2a0a688acd7edbf0a5ec18915171cd7f2f

Use synthetic event for 'Network.getResponseBody'

lib/live.js
@@ -94,6 +94,27 @@ class Live {
             client.on('event', (event) => {
                 stats.processEvent(fulfill, reject, event);
             });
+            // XXX the separation of concerns between live fetching and HAR
+            // computation made it necessary to introduce a synthetic event
+            // which is the reply of the Network.getResponseBody method
+            if (this._options.content) {
+                Network.loadingFinished(async ({requestId}) => {
+                    try {
+                        const params = await Network.getResponseBody({requestId});
+                        const {body, base64Encoded} = params;
+                        stats.processEvent(fulfill, reject, {
+                            method: 'Network.getResponseBody',
+                            params: {
+                                requestId,
+                                body,
+                                base64Encoded
+                            }
+                        });
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
+            }
         });
         // start the page load
         const navigation = Page.navigate({url: this._url});

lib/stats.js
@@ -122,24 +122,6 @@ class Stats {
         }
         entry.encodedResponseLength = encodedDataLength;
         entry.responseFinishedS = timestamp;
-
-
-        // TODO handle this
-
-        // // optionally fetch the entry content
-        // if (this._options.content) {
-        //     try {
-        //         const params = await Network.getResponseBody({requestId});
-        //         const {body, base64Encoded} = params;
-        //         entry.responseBody = body;
-        //         entry.responseBodyIsBase64 = base64Encoded;
-        //     } catch (err) {
-        //         reject(err);
-        //         return;
-        //     }
-        // }
-
-
     }
 
     _Network_loadingFailed(fulfill, reject, params) {
@@ -148,6 +130,16 @@ class Stats {
             const message = errorText || canceled && 'Canceled';
             reject(new Error(message));
         }
+    }
+
+    _Network_getResponseBody(fulfill, reject, params) {
+        const {requestId, body, base64Encoded} = params;
+        const entry = this.entries.get(requestId);
+        if (!entry) {
+            return;
+        }
+        entry.responseBody = body;
+        entry.responseBodyIsBase64 = base64Encoded;
     }
 }
 

commit 6e7a042cce3d182658661281c2703e81eec1bb11

Execute 'Network.getResponseBody' only for currently tracked entries
Bug introduced in 15f0de2a0a688acd7edbf0a5ec18915171cd7f2f where the check was
removed.

Fixes #64.

lib/live.js
@@ -99,6 +99,11 @@ class Live {
             // which is the reply of the Network.getResponseBody method
             if (this._options.content) {
                 Network.loadingFinished(async ({requestId}) => {
+                    // only for those entries that are being tracked (e.g., not
+                    // for cached items)
+                    if (!stats.entries.get(requestId)) {
+                        return;
+                    }
                     try {
                         const params = await Network.getResponseBody({requestId});
                         const {body, base64Encoded} = params;